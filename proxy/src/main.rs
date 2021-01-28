21196d6dd213b8a55200ee714dd1ec6bd25fe1eb
info@mgr.moe <info@mgr.moe>
2021-01-28 16:46:59 +0100

    formatted.

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -4,31 +4,27 @@ use tokio_stream::StreamExt;
 
 use futures::FutureExt;
 use std::error::Error;
-use tokio::sync::RwLock;
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
-use tokio::io::BufReader;
-use tokio::io::AsyncWriteExt;
 use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::io::BufReader;
+use tokio::sync::RwLock;
 
 struct Ip {
     ip_addr: String,
     expire: SystemTime,
 }
 
-async fn proxy(ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>,listen_addr: String, server_addr: String) {
+async fn proxy(ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>, listen_addr: String, server_addr: String) {
     println!("proxying {} -> {}", listen_addr, server_addr);
-    
     let listener = TcpListener::bind(listen_addr).await.unwrap();
 
     while let Ok((inbound, _)) = listener.accept().await {
         let ip_addr = inbound.peer_addr().unwrap().ip().to_string();
         let buf = ips.read().await;
-        let mut ip_find = tokio_stream::iter(buf.iter()).filter(
-            |x| futures::executor::block_on(async {
-                x.read().await.ip_addr == ip_addr 
-            }  
-            ));
+        let mut ip_find = tokio_stream::iter(buf.iter())
+            .filter(|x| futures::executor::block_on(async { x.read().await.ip_addr == ip_addr }));
         if let Some(ip) = ip_find.next().await {
             let transfer = transfer(Arc::clone(ip), inbound, server_addr.clone()).map(|r| {
                 if let Err(e) = r {
@@ -42,31 +38,25 @@ async fn proxy(ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>,listen_addr: String, serve
 fn remove_whitespace(s: &mut String) {
     s.retain(|c| !c.is_whitespace());
 }
-async fn admin(ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>){
+async fn admin(ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>) {
     let listener = TcpListener::bind("127.0.0.1:1337").await.unwrap();
     while let Ok((inbound, _)) = listener.accept().await {
         let mut reader = BufReader::new(inbound);
         let mut line = String::new();
         reader.read_line(&mut line).await.unwrap();
         remove_whitespace(&mut line);
-        println!("{}",line);
+        println!("{}", line);
         let buf = ips.read().await;
-        let mut ip_find = tokio_stream::iter(buf.iter()).filter(
-            |x| futures::executor::block_on(async {
-                x.read().await.ip_addr == line 
-            }  
-            ));
-            if let Some(ip) = ip_find.next().await {
-                ip.write().await.expire = SystemTime::now() + Duration::from_secs(30);
-            }else{
-                ips.write().await.push(Arc::new(RwLock::new(Ip {
-                    ip_addr: line,
-                    expire: SystemTime::now() + Duration::from_secs(30),
-                })));
-            }
-
-        
-        
+        let mut ip_find = tokio_stream::iter(buf.iter())
+            .filter(|x| futures::executor::block_on(async { x.read().await.ip_addr == line }));
+        if let Some(ip) = ip_find.next().await {
+            ip.write().await.expire = SystemTime::now() + Duration::from_secs(30);
+        } else {
+            ips.write().await.push(Arc::new(RwLock::new(Ip {
+                ip_addr: line,
+                expire: SystemTime::now() + Duration::from_secs(30),
+            })));
+        }
     }
 }
 
@@ -75,11 +65,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut handles = vec![];
     let ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>> = Arc::new(RwLock::new(Vec::new()));
 
-    handles.push(tokio::spawn(proxy(ips.clone(),
+    handles.push(tokio::spawn(proxy(
+        ips.clone(),
         "0.0.0.0:13001".to_string(),
         "192.168.178.130:13001".to_string(),
     )));
-    handles.push(tokio::spawn(proxy(ips.clone(),
+    handles.push(tokio::spawn(proxy(
+        ips.clone(),
         "0.0.0.0:8001".to_string(),
         "192.168.178.130:8001".to_string(),
     )));
@@ -96,14 +88,11 @@ async fn transfer(
     if ip.read().await.expire < SystemTime::now() {
         return Err(std::io::Error::new(
             std::io::ErrorKind::Other,
-            format!(
-                "{} tried to connect without auth",
-                ip.read().await.ip_addr
-            ),
+            format!("{} tried to connect without auth", ip.read().await.ip_addr),
         )
         .into());
     }
-    println!("{} connected",ip.read().await.ip_addr);
+    println!("{} connected", ip.read().await.ip_addr);
     let mut outbound = TcpStream::connect(proxy_addr).await?;
 
     let (mut ri, mut wi) = inbound.split();
@@ -119,20 +108,18 @@ async fn transfer(
     };
     let ip_exp = async {
         loop {
-            let lock =ip.read().await; 
+            let lock = ip.read().await;
             if lock.expire < SystemTime::now() {
                 return Err(std::io::Error::new(
                     std::io::ErrorKind::Other,
                     format!("{} expired", ip.read().await.ip_addr),
                 ));
             }
-            
             tokio::time::sleep(Duration::from_secs(5)).await;
         }
         return Ok(());
     };
 
     tokio::try_join!(client_to_server, server_to_client, ip_exp)?;
-    
     return Ok(());
 }
