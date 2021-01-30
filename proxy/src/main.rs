c140b088eb945dca9b752a7ee5f72ec88da76258
XNiveres <info@mgr.moe>
2021-01-30 03:04:36 +0100

    fuck filter_maps! for loop fixxes lock issues!

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,6 +1,7 @@
 use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
 
+use futures::stream::{self, StreamExt};
 use futures::FutureExt;
 use std::error::Error;
 use std::sync::Arc;
@@ -9,7 +10,6 @@ use tokio::io::AsyncBufReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
 use tokio::sync::RwLock;
-use futures::stream::{self, StreamExt};
 
 struct Ip {
     ip_addr: String,
@@ -23,17 +23,16 @@ async fn proxy(ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>, listen_addr: String, serv
     while let Ok((inbound, _)) = listener.accept().await {
         let ip_addr = inbound.peer_addr().unwrap().ip().to_string();
         let buf = ips.read().await;
-        let mut ip_find = stream::iter(buf.iter())
-        .filter_map(|x| {
+        let mut ip_find = stream::iter(buf.iter()).filter_map(|x| {
             let ip_addr = &ip_addr;
-            async move{ 
-            if x.read().await.ip_addr == ip_addr.to_string() {
-                Some(x)
-            } else {
-                None
+            async move {
+                if x.read().await.ip_addr == ip_addr.to_string() {
+                    Some(x)
+                } else {
+                    None
+                }
             }
-        }}
-    );
+        });
         tokio::pin!(ip_find);
         if let Some(ip) = ip_find.next().await {
             let transfer = transfer(Arc::clone(ip), inbound, server_addr.clone()).map(|r| {
@@ -49,46 +48,38 @@ fn remove_whitespace(s: &mut String) {
     s.retain(|c| !c.is_whitespace());
 }
 
-
-async fn process(mut ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>,inbound: TcpStream){
+async fn process(mut ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>, inbound: TcpStream) {
     let mut reader = BufReader::new(inbound);
-            let mut line = String::new();
-            reader.read_line(&mut line).await.unwrap();
-            remove_whitespace(&mut line);
-            println!("adding {}", line);
-            let ip_cop: String = line.clone();
-            let ip_buf = ips.read().await;
-            let mut ip_find = stream::iter(ip_buf.iter())
-            .filter_map(|x| {
-                let ip_addr = &line;
-                async move{ 
-                if x.read().await.ip_addr == ip_addr.to_string() {
-                    Some(x)
-                } else {
-                    None
-                }
-            }}
-        );
-            tokio::pin!(ip_find);
-            if let Some(ip) = ip_find.next().await {
-                ip.write().await.expire = SystemTime::now() + Duration::from_secs(30);
-            }else{
-                println!("reaches");
-                ips.write().await.push(Arc::new(RwLock::new(Ip {
-                    ip_addr: ip_cop,
-                    expire: SystemTime::now() + Duration::from_secs(30),
-                })));
-                println!("not reached");
-            }
+    let mut line = String::new();
+    reader.read_line(&mut line).await.unwrap();
+    remove_whitespace(&mut line);
+    println!("adding {}", line);
+    let ip_cop: String = line.clone();
+    let mut found: bool = false;
+    let mut index: usize = 0;
+    let ip_addr = &line;
 
-
-           
+    for n in 0..ips.read().await.len(){
+        if ips.read().await[n].read().await.ip_addr == ip_addr.to_string(){
+            index = n;
+            found = true;
+            break;
+        }
+    }
+    if found {
+        ips.write().await[index].write().await.expire = SystemTime::now() + Duration::from_secs(30);
+        return;
+    }
+    ips.write().await.push(Arc::new(RwLock::new(Ip {
+        ip_addr: ip_cop,
+        expire: SystemTime::now() + Duration::from_secs(30),
+    })));
+    println!("not reached");
 }
 async fn admin(ips: Arc<RwLock<Vec<Arc<RwLock<Ip>>>>>) {
     let listener = TcpListener::bind("0.0.0.0:1337").await.unwrap();
     while let Ok((inbound, _)) = listener.accept().await {
         tokio::spawn(process(ips.clone(), inbound));
-        
     }
 }
 
